80732e9cce3f7accf7b36e718871f10d8d949516
Piao Ya <47481422+piaoyapia@users.noreply.github.com>
2019-02-22 23:12:58 +0100

    Css + model

--- app/server.py
+++ app/server.py
@@ -8,8 +8,8 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1sssFZ3CoTZqkVtAwjFfFM62cS9l87DSI'
+export_file_name = 'export_plastics.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
