3f02791ab077f88c7b808a6814c5fd945f6e66d0
Piao Ya <47481422+piaoyapia@users.noreply.github.com>
2019-02-23 14:56:34 +0100

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -46,7 +46,7 @@ console.log(selected);
 }
 
 $.ajax({
-  url: 'https://raw.githubusercontent.com/F-R-S/Plastic_RIC/master/app/static/data.csv',
+  url: 'https://raw.githubusercontent.com/piaoyapia/Plastic_RIC/master/app/static/data.csv',
   dataType: 'text',
 }).done(print_data);
 //successFunction
