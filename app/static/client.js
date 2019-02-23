1ea320de7ab523333d702ea4c7f94051831e1058
F-R-S <47916483+F-R-S@users.noreply.github.com>
2019-02-23 16:24:10 +0100

    § statt ,

--- app/static/client.js
+++ app/static/client.js
@@ -57,7 +57,7 @@ function print_data(data){
   // Selektiert die Reihen
   var allRows = data.split(/\r?\n|\r/);
   // Teilt die erste Zeile
-  var titel = allRows[0].split(',');
+  var titel = allRows[0].split('§');
   // Wählt den ersten Eintrag der Ersten Zeile und schreibt in HTML
   el('info_general_titel').innerHTML = titel[0];
   el('info_market_titel').innerHTML = titel[1];
