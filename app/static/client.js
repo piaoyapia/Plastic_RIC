0d7b75e0d4741ea6b3d56a747cd426cb1209dccf
Piao Ya <47481422+piaoyapia@users.noreply.github.com>
2019-02-27 11:29:47 +0100

    change

--- app/static/client.js
+++ app/static/client.js
@@ -54,7 +54,7 @@ function analyze() {
 
 //neu:
 
-function chooseResult(result){
+function chooseResult('result'){
   var plastics_origin= ['1_polyethylene_PET', '2_high_density_polyethylene_PE-HD', '3_polyvinylchloride_PVC', '4_low_density_polyethylene_PE-LD', '5_polypropylene_PP','6_polystyrene_PS', '7_other_resins'];
   var pos=plastics_origin.indexOf('result');
 
