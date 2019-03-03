889f2182530479f36e8d13bd24b95a076b024fb3
Piao Ya <47481422+piaoyapia@users.noreply.github.com>
2019-03-03 14:17:56 +0100

    revert?

--- app/static/client.js
+++ app/static/client.js
@@ -4,71 +4,75 @@ launchmode = 1;
 
 //Variable
 var el = x => document.getElementById(x);
-//try to hide
+
+
+
+function set_selection() {
+  selection = 1;
+
+  //try to hide
   $('.info-box').hide();
 
 
-function set_selection()
-{
-selection = 1;
-
-
 }
 
-function showPicker(inputId) { el('file-input').click();
-data_output("1_polyethylene_PE");
- }
+function showPicker(inputId) {
+  el('file-input').click();
+  data_output("1_polyethylene_PE");
+}
 
 function showPicked(input) {
-    el('upload-label').innerHTML = input.files[0].name;
-    var reader = new FileReader();
-    reader.onload = function (e) {
-        el('image-picked').src = e.target.result;
-        el('image-picked').className = '';
-    }
-    reader.readAsDataURL(input.files[0]);
+  el('upload-label').innerHTML = input.files[0].name;
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    el('image-picked').src = e.target.result;
+    el('image-picked').className = '';
+  }
+  reader.readAsDataURL(input.files[0]);
 }
 
 function analyze() {
-    var uploadFiles = el('file-input').files;
-    if (uploadFiles.length != 1) alert('Please select 1 file to analyze!');
+  var uploadFiles = el('file-input').files;
+  if (uploadFiles.length != 1) alert('Please select 1 file to analyze!');
 
-    el('analyze-button').innerHTML = 'Analyze...';
-    var xhr = new XMLHttpRequest();
-    var loc = window.location
-    xhr.open('POST', `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`, true);
-    xhr.onerror = function() {alert (xhr.responseText);}
-    xhr.onload = function(e) {
-        if (this.readyState === 4) {
-            var response = JSON.parse(e.target.responseText);
+  el('analyze-button').innerHTML = 'Analyze...';
+  var xhr = new XMLHttpRequest();
+  var loc = window.location
+  xhr.open('POST', `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`, true);
+  xhr.onerror = function() {
+    alert(xhr.responseText);
+  }
+  xhr.onload = function(e) {
+    if (this.readyState === 4) {
+      var response = JSON.parse(e.target.responseText);
 
-            el('result-label').innerHTML = `${'The identified Plastic is ' + response['result']}`;
+      el('result-label').innerHTML = `${'The identified Plastic is ' + response['result']}`;
 
-            var response_for_selection = `${response['result']}`;
-            var split = response_for_selection.split("_",1);
-            selection = split[0];
-            update_text();
-            // try to show
-                    $('.info-box').show();
-
-        }
-        el('analyze-button').innerHTML = 'Analyse';
-
-
+      var response_for_selection = `${response['result']}`;
+      var split = response_for_selection.split("_", 1);
+      selection = split[0];
+      update_text();
     }
+    el('analyze-button').innerHTML = 'Analyse';
 
-    var fileData = new FormData();
-    fileData.append('file', uploadFiles[0]);
-    xhr.send(fileData);
+    // try to show
+    $('.info-box').show();
+
+
+  }
+
+  var fileData = new FormData();
+  fileData.append('file', uploadFiles[0]);
+  xhr.send(fileData);
 }
 
 
 
-function data_output(selected){
+function data_output(selected) {
 
-if(selected = "1_polyethylene_PET" ){
-console.log(selected);
-}
+  if (selected = "1_polyethylene_PET") {
+    console.log(selected);
+  }
 }
 
 $.ajax({
@@ -77,38 +81,36 @@ $.ajax({
 }).done(print_data);
 //successFunction
 
-function print_data(data){
-all_data = data;
+function print_data(data) {
+  all_data = data;
 }
 
-function update_text(){
-if(selection != 99){
-/// constant titles --> csv headlines
-  // rows get selected
-  var allRows = all_data.split(/\r?\n|\r/);
-  // Splits the first row
-  var title = allRows[0].split('§');
-  // Chooses the first/second/third/forth entry of the first row and writes it in HTML
-  el('info_general_title').innerHTML = title[0];
-  el('info_market_title').innerHTML = title[1];
-  el('info_recycling_title').innerHTML = title[2];
-  el('info_alternatives_title').innerHTML = title[3];
-  // <--- stays the same for every possible content.
+function update_text() {
+  if (selection != 99) {
+    /// constant titles --> csv headlines
+    // rows get selected
+    var allRows = all_data.split(/\r?\n|\r/);
+    // Splits the first row
+    var title = allRows[0].split('§');
+    // Chooses the first/second/third/forth entry of the first row and writes it in HTML
+    el('info_general_title').innerHTML = title[0];
+    el('info_market_title').innerHTML = title[1];
+    el('info_recycling_title').innerHTML = title[2];
+    el('info_alternatives_title').innerHTML = title[3];
+    // <--- stays the same for every possible content.
 
 
-  // The choosen content: Splits the rows of the entry:
-  var content = allRows[selection].split('§');
-  // Choose the first/second/third/forth column entry of the choosen row.
-  //Row gets choosen by the structure of the data.csv (4x4 table)
-  el('info_general_content').innerHTML = content[0];
-  el('info_market_content').innerHTML = content[1];
-  el('info_recycling_content').innerHTML = content[2];
-  el('info_alternatives_content').innerHTML = content[3];
+    // The choosen content: Splits the rows of the entry:
+    var content = allRows[selection].split('§');
+    // Choose the first/second/third/forth column entry of the choosen row.
+    //Row gets choosen by the structure of the data.csv (4x4 table)
+    el('info_general_content').innerHTML = content[0];
+    el('info_market_content').innerHTML = content[1];
+    el('info_recycling_content').innerHTML = content[2];
+    el('info_alternatives_content').innerHTML = content[3];
 
-  //Update label needed for analysis:
-  el('result-label').innerHTML = `${'The identified Plastic is ' + content[0]}`;
+    //Update label needed for analysis:
+    el('result-label').innerHTML = `${'The identified Plastic is ' + content[0]}`;
 
-
-
-}
+  }
 }
