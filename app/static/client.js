c10f773b24d2597a28ea794a30b99d3f4bd16b08
Piao Ya <47481422+piaoyapia@users.noreply.github.com>
2020-02-03 23:27:44 +0100

    implementing launchmode

--- app/static/client.js
+++ app/static/client.js
@@ -1,6 +1,6 @@
 //Constant
 // 0 = Debug Mode; 1 = Launchmode
-launchmode = 1;
+launchmode = 0;
 
 //Variable
 var el = x => document.getElementById(x);
@@ -14,6 +14,21 @@ function set_selection() {
     $('.analyze-button').hide();
     $('.result-label').hide();
   }
+
+  if (launchmode ==0){
+    var ueberschrift = "Dies ist Überschrift.";
+    var text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labor.";
+    el("info_general_title").innerHTML = ueberschrift;
+    el("info_market_title").innerHTML = ueberschrift;
+    el("info_recycling_title").innerHTML = ueberschrift;
+    el("info_alternatives_title").innerHTML = ueberschrift;
+    el("info_general_content").innerHTML = text;
+    el("info_market_content").innerHTML = text;
+    el("info_recycling_content").innerHTML = text;
+    el("info_alternatives_content").innerHTML = text;
+    el("result-label").innerHTML = "Das ist PET.";
+  }
+
 }
 
 function showPicker(inputId) {
@@ -49,12 +64,12 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
 
-      // Restart Analizing
+      // Restart Analyzing
       el("analyze-button").innerHTML = "Restart Analysis";
 
       var response = JSON.parse(e.target.responseText);
-      /* el("result-label").innerHTML = `${response["result"]}`; */
 
+      //später hier debuggen: Resultlabel in Antwort umwandeln
       el("result-label").innerHTML = `${response["result"]}`;
       var response_for_selection = `${response["result"]}`;
       var split = response_for_selection.split("_", 1);
