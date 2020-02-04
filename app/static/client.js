813ddbb1bfae43c58ddec9072ebf1321c96a653e
Piao Ya <47481422+piaoyapia@users.noreply.github.com>
2020-02-04 18:15:11 +0100

    delete add on

--- app/static/client.js
+++ app/static/client.js
@@ -55,42 +55,6 @@ function analyze() {
   var uploadFiles = el("file-input").files;
   // hier implementieren, dass das Bild schon quadratisch zugeschnitten wird.
 
-                    function handleFileSelect(evt) {
-                      var files = evt.target.files; // FileList object
-
-                      // Loop through the FileList and render image files as thumbnails.
-                      for (var i = 0, f; f = files[i]; i++) {
-
-                        // Only process image files.
-                        if (!f.type.match('image.*')) {
-                          continue;
-                        }
-                        
-
-                        var reader = new FileReader();
-
-                        // Closure to capture the file information.
-                        reader.onload = (function(theFile) {
-                          return function(e) {
-                            // Render thumbnail.
-                            var span = document.createElement('span');
-                            span.innerHTML = ['<img class="thumb" src="', e.target.result,
-                                              '" title="', escape(theFile.name), '"/>'].join('');
-                            document.getElementById('list').insertBefore(span, null);
-                          };
-                        })(f);
-
-                        // Read in the image file as a data URL.
-                        reader.readAsDataURL(f);
-                      }
-                    }
-
-                    document.getElementById('files').addEventListener('change', handleFileSelect, false);
-
-
-
-
-  //---_> Everythings fine:
   if (uploadFiles.length != 1) alert("Please select one picture to analyze.");
 
   var xhr = new XMLHttpRequest();
